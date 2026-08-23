80d04549f21e5c143bf36ec137a3e809e1f46d88
Sayma S1 <samiawork6@gmail.com>
2026-08-23 23:40:30 +0600

    Update and rename yatriSayma.txt to sos_full.c

--- sos_full.c
+++ sos_full.c
@@ -1,32 +1,31 @@
 #include<stdio.h>
+
 int sos_alert = 0;
 int is_Active_Passenger = 0;
+
 void trigger_sos_alert(){
-    if (! is_Active_Passenger){
-        printf("No active passenger.\n  No SOS alert can be triggered.\n");
-        return;
+if (!is_Active_Passenger){
+printf("No active passenger.\n  No SOS alert can be triggered.\n");
+  return;
     }
 
-
-    
     printf("\n============================================\n");
     printf("\n[!!!]EMERGENCY SOS ALERT TRIGGERED [!!!]\n");
     printf("============================================\n");
 
-
     printf("contacting emergency services...\n");
-    printf("C Immediately Local Authorities \n");
+    printf("Contact Immediately Local Authorities \n");
     printf(" Police           : 999\n");
     printf(" Ambulance        : 999\n");
     printf("Fire Service      :999\n");
-   
+
     sos_alert = 1;
     printf("==============================================\n");
     printf("SOS alert has been triggered successfully.\n");
-    printf("===============================================\n")
-  
+    printf("===============================================\n");
+}
 
-   int main(){
+int main(){
     int choice;
 
     printf("\n==========================================\n");
@@ -44,4 +43,4 @@ void trigger_sos_alert(){
         printf("Exiting Program....\n");
     }
     return 0;
-   }
+}
